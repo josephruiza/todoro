e544b645286862227c7412f3b02e7722b14adc12
Joseph Ruiz <joseph.ruiz.a@gmail.com>
2017-03-19 12:15:34 +0100

    Se añade modelo de Task

--- src/tasks/models.py
+++ src/tasks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    PENDING = "PEN"
+    DONE = "DON"
+    STATUSES = (
+        (PENDING, "Pending"),
+        (DONE, "Done")
+    )
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=3, default=PENDING, choices=STATUSES)
+    time_estimated = models.IntegerField(null=True)
+    deadline = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True) #automaticamente añada la fecha de creación
+    modified_at = models.DateTimeField(auto_now=True) #automaticamente actualiza la fecha
+
