383cbca24d6490d5475765413bc4d9838e7222c4
Joseph Ruiz <joseph.ruiz.a@gmail.com>
2017-03-20 01:26:53 +0100

    Se añaden relaciones de Task y migraciones

--- src/tasks/models.py
+++ src/tasks/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -12,10 +13,12 @@ class Task(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     status = models.CharField(max_length=3, default=PENDING, choices=STATUSES)
-    time_estimated = models.IntegerField(null=True)
-    deadline = models.DateTimeField(null=True)
+    time_estimated = models.IntegerField(blank=True,null=True)
+    deadline = models.DateTimeField(blank=True,null=True)
     created_at = models.DateTimeField(auto_now_add=True) #automaticamente añada la fecha de creación
     modified_at = models.DateTimeField(auto_now=True) #automaticamente actualiza la fecha
+    owner = models.ForeignKey(User, related_name="own_tasks")
+    assigned = models.ForeignKey(User, related_name="assigned_tasks", null=True, default=None)
 
     def __str__(self):
         return self.name
